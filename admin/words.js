f8613e76963af088f04118a797db799a9b1c0453
cmoehler <ccm@gmx.net>
2022-03-11 10:53:10 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'leackagedect adapter settings': {
+		'en': 'Adapter settings for leackagedect',
+		'de': 'Adaptereinstellungen für leackagedect',
+		'ru': 'Настройки адаптера для leackagedect',
+		'pt': 'Configurações do adaptador para leackagedect',
+		'nl': 'Adapterinstellingen voor leackagedect',
+		'fr': "Paramètres d'adaptateur pour leackagedect",
+		'it': "Impostazioni dell'adattatore per leackagedect",
+		'es': 'Ajustes del adaptador para leackagedect',
+		'pl': 'Ustawienia adaptera dla leackagedect',
+		'zh-cn': 'leackagedect的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'вариант 2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
