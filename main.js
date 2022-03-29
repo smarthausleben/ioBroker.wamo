ed36390d7a7c34a53a253bbdb62e60a8cc12b681
cmoehler <info@smarthausleben.de>
2022-03-29 18:42:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1615,7 +1615,7 @@ class wamo extends utils.Adapter {
 
 		try{
 			const test = await this.getStateAsync(adapterChannels.DeviceInfo.path + '.' + DeviceParameters.IPAddress.id);
-			this.log.warn('state resolved: ' + String(test));
+			this.log.warn('state resolved: ' + test.val);
 		}catch(err){
 			this.log.warn('state rejected');
 		}
