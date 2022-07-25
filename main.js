eea6881e6d6cc392907c4a1d598935d1eacf7cff
cmoehler <info@smarthausleben.de>
2022-07-25 09:22:40 +0200

    subscribeStates SRO (screen rotation)

--- main.js
+++ main.js
@@ -3247,11 +3247,11 @@ class wamo extends utils.Adapter {
 		*/
 
 		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		// this.subscribeStates(DeviceParameters.ScreenRotation.statePath + '.' + DeviceParameters.ScreenRotation);
+		this.subscribeStates(DeviceParameters.ScreenRotation.statePath + '.' + DeviceParameters.ScreenRotation.id);
 		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
 		// this.subscribeStates('lights.*');
 		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-		this.subscribeStates('*');
+		// this.subscribeStates('*');
 
 		/*
 			setState examples
