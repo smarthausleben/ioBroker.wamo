e4ac0f399e7f6c07e6b2c797460431ae9e27df50
cmoehler <info@smarthausleben.de>
2022-07-26 11:31:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3361,32 +3361,32 @@ class wamo extends utils.Adapter {
 						break;
 				}
 			}
-			else if((id.includes(statePrefix + '.' + adapterChannels.DevicePofiles1.path)) && (state.ack == false)){
+			else if((id.includes(statePrefix + adapterChannels.DevicePofiles1.path)) && (state.ack == false)){
 				this.log.warn('Profile 1 State Change: ' + String(id));
 			}
-			else if((id.includes(statePrefix + '.' + adapterChannels.DevicePofiles2.path)) && (state.ack == false)){
+			else if((id.includes(statePrefix + adapterChannels.DevicePofiles2.path)) && (state.ack == false)){
 				this.log.warn('Profile 2 State Change: ' + String(id));
 			}
-			else if((id.includes(statePrefix + '.' + adapterChannels.DevicePofiles3.path)) && (state.ack == false)){
+			else if((id.includes(statePrefix + adapterChannels.DevicePofiles3.path)) && (state.ack == false)){
 				this.log.warn('Profile 3 State Change: ' + String(id));
 			}
-			else if((id.includes(statePrefix + '.' + adapterChannels.DevicePofiles4.path)) && (state.ack == false)){
+			else if((id.includes(statePrefix + adapterChannels.DevicePofiles4.path)) && (state.ack == false)){
 				this.log.warn('Profile 4 State Change: ' + String(id));
 			}
-			else if((id.includes(statePrefix + '.' + adapterChannels.DevicePofiles5.path)) && (state.ack == false)){
+			else if((id.includes(statePrefix + adapterChannels.DevicePofiles5.path)) && (state.ack == false)){
 				this.log.warn('Profile 5 State Change: ' + String(id));
 			}
-			else if((id.includes(statePrefix + '.' + adapterChannels.DevicePofiles6.path)) && (state.ack == false)){
+			else if((id.includes(statePrefix + adapterChannels.DevicePofiles6.path)) && (state.ack == false)){
 				this.log.warn('Profile 6 State Change: ' + String(id));
 			}
-			else if((id.includes(statePrefix + '.' + adapterChannels.DevicePofiles7.path)) && (state.ack == false)){
+			else if((id.includes(statePrefix + adapterChannels.DevicePofiles7.path)) && (state.ack == false)){
 				this.log.warn('Profile 7 State Change: ' + String(id));
 			}
-			else if((id.includes(statePrefix + '.' + adapterChannels.DevicePofiles8.path)) && (state.ack == false)){
+			else if((id.includes(statePrefix + adapterChannels.DevicePofiles8.path)) && (state.ack == false)){
 				this.log.warn('Profile 8 State Change: ' + String(id));
 			}
 			else{
-				this.log.warn('StateChange: ' + String(id));
+				this.log.warn('StateChange: ' + String(id) + ' Value: ' + String(state.val) + ' acknowledged: ' + String(state.ack));
 			}
 
 		} else {
