d04b3012ac7ee4b97aa5ca54ce1ac3aef4822d0c
cmoehler <info@smarthausleben.de>
2022-08-08 15:11:10 +0200

    removed doResetHistory

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require('@iobroker/adapter-core');
-const { doResetHistory } = require('@iobroker/testing/build/tests/unit/mocks/tools');
+//const { doResetHistory } = require('@iobroker/testing/build/tests/unit/mocks/tools');
 const axios = require('axios').default;
 const http = require('http');
 const schedule = require('node-schedule');
