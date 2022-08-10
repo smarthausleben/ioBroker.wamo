2e6d59ca2548c9b6fb26f2423d808b9d82c1b2b3
cmoehler <info@smarthausleben.de>
2022-08-10 14:32:03 +0200

    added state SLE - Self learning time to end

--- main.js
+++ main.js
@@ -2086,6 +2086,13 @@ class wamo extends utils.Adapter {
 					}
 					if (valuesInfoMessages) { await this.moremessages(DeviceParameters.SelfLearningMinimumFlow, finalValue); }
 					break;
+				case DeviceParameters.SelfLearningTimeToEnd.id:		// SLE - Self learning time to end
+					finalValue = await this.getGlobalisedValue(DeviceParameters.SelfLearningTimeToEnd, value);
+					if (finalValue === null) {	// did we get a globalised Value back?
+						finalValue = value;
+					}
+					if (valuesInfoMessages) { await this.moremessages(DeviceParameters.SelfLearningTimeToEnd, finalValue); }
+					break;
 				default:
 					this.log.warn('[async convertDeviceReturnValue(valueKey, value)] Key (' + String(valueKey) + ') is not valid!');
 					finalValue = value;
