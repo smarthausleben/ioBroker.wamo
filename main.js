1372fec335bc027b3c613533c2c1c4b2625b5612
cmoehler <info@smarthausleben.de>
2022-08-16 09:07:07 +0200

    create restart state fix

--- main.js
+++ main.js
@@ -1579,7 +1579,7 @@ class wamo extends utils.Adapter {
 		try {
 			for (const key in deviceControlStates) {
 				try {
-					await this.setObjectNotExistsAsync(deviceControlStates[key].statePath + + '.' + DeviceParameters[key].id, Object(deviceControlStates[key].objectdefinition));
+					await this.setObjectNotExistsAsync(deviceControlStates[key].statePath + '.' + DeviceParameters[key].id, Object(deviceControlStates[key].objectdefinition));
 					this.log.debug('Device control state ' + deviceControlStates[key].id + ' created');
 				} catch (err) {
 					this.log.error('[async createDeviceControlStates()] ERROR Channel: ]' + err);
