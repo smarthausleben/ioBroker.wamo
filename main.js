55dc01d87e761d6ca6ced049e7a1b204ea4b1785
cmoehler <info@smarthausleben.de>
2023-04-19 18:33:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1418,12 +1418,11 @@ class wamo extends utils.Adapter {
 		try {
 			// iterate through all requested Parameters
 			for (const key in DeviceParameters) {
-				if (LP_Data[key] != undefined) {
-					if (DeviceParameters[key].saveRawData && LP_Data[String(DeviceParameters[key].id)] !== undefined) {
-						this.log.warn('Raw data integration');
+				if (LP_Data['get' + DeviceParameters[key].id] != undefined) {
+					if (DeviceParameters[key].saveRawData && LP_Data['get' + String(DeviceParameters[key].id)] !== undefined) {
+						this.log.warn('Raw Data storage needs integration!!!');
 					}
-
-					this.log.warn('this.convertDeviceReturnValue() return: ' + String(this.convertDeviceReturnValue(key, LP_Data[key])));
+					this.log.warn('this.convertDeviceReturnValue() return: ' + String(this.convertDeviceReturnValue(String(DeviceParameters[key].id), LP_Data['get' + DeviceParameters[key].id])));
 				}
 				else {
 					this.log.warn('Parameter ' + String(key) + ' is not implemented yet.');
