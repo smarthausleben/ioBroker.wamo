61f1946d901d9aa79b8314dc06f79e61955ec906
cmoehler <info@smarthausleben.de>
2023-05-10 10:24:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4386,7 +4386,7 @@ class wamo extends utils.Adapter {
 			const endfinal = end - 1;
 			tmp = tmp.substring(1, endfinal);
 			this.log.warn(tmp);
-			const Alarms = tmp.split('\r\n');
+			const Alarms = tmp.split('\\r\\n');
 			this.log.warn('Array Length after Splitt = ' + String(Alarms.length));
 			if (Alarms != null && Alarms.length > 0) {
 				for (let z = 0; z < Alarms.length; z++) {
