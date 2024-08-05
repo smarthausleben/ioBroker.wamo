0e543d8bd62567fa30d2e49c04175af9682df685
cmoehler <info@smarthausleben.de>
2024-08-05 18:30:50 +0200

    update eslind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import { includeIgnoreFile } from "@eslint/compat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,10 +13,11 @@ const compat = new FlatCompat({
     recommendedConfig: js.configs.recommended,
     allConfig: js.configs.all
 });
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default [{
     ignores: ["**/.eslintrc.js"],
-}, ...compat.extends("eslint:recommended", "plugin:react/recommended"), {
+}, includeIgnoreFile(gitignorePath), ...compat.extends("eslint:recommended", "plugin:react/recommended"), {
     plugins: {
         react,
     },
