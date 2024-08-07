bc31535b9225cf0471a2555133686fc0338a2808
cmoehler <info@smarthausleben.de>
2024-08-07 10:11:28 +0200

    housekeeping

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -4,40 +4,37 @@ const js = require("@eslint/js");
 const { FlatCompat } = require("@eslint/eslintrc");
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 module.exports = [
-    {
-        ignores: [
-          ".dev-server/**",
-          "**/admin/words.js"
-        ],
-    },
-    ...compat.extends("eslint:recommended", "plugin:prettier/recommended"),
-    {
-        languageOptions: {
-            globals: {
-                ...globals.node,
-                ...globals.mocha,
-            },
+  {
+    ignores: [".dev-server/**", "**/admin/words.js"],
+  },
+  ...compat.extends("eslint:recommended", "plugin:prettier/recommended"),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.mocha,
+      },
 
-            ecmaVersion: 2022,
-            sourceType: "commonjs",
+      ecmaVersion: 2022,
+      sourceType: "commonjs",
 
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
-
-        rules: {
-            "no-console": "off",
-            "no-var": "error",
-            "prefer-const": "error",
-        },
+      },
     },
+
+    rules: {
+      "no-console": "off",
+      "no-var": "error",
+      "prefer-const": "error",
+    },
+  },
 ];
